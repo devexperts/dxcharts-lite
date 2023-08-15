82d4dd89e11eeea867d0c5e152cab3777c513c8c
Georgii Savin <gsavin@devexperts.com>
2023-08-15 22:04:14 +0300

    fix: draw y-axis highlight for multiple panes & y-axis

--- src/chart/components/y_axis/price_labels/y-axis-price-labels.drawer.ts
+++ src/chart/components/y_axis/price_labels/y-axis-price-labels.drawer.ts
@@ -10,9 +10,8 @@ import {
 	limitYToBounds,
 } from '../../../canvas/canvas-bounds-container';
 import { ChartConfigComponentsYAxis, FullChartColors } from '../../../chart.config';
-import { CanvasModel } from '../../../model/canvas.model';
 import { Drawer } from '../../../drawers/drawing-manager';
-import { Bounds } from '../../../model/bounds.model';
+import { CanvasModel } from '../../../model/canvas.model';
 import { fillRect } from '../../../utils/canvas/canvas-drawing-functions.utils';
 import { drawLabel } from './price-label.drawer';
 import { LabelGroup, VisualYAxisLabel } from './y-axis-labels.model';
@@ -34,7 +33,7 @@ export class YAxisPriceLabelsDrawer implements Drawer {
 
 		const yAxisBounds = this.canvasBoundsContainer.getBounds(CanvasElement.PANE_UUID_Y_AXIS(CHART_UUID));
 		const paneBounds = this.canvasBoundsContainer.getBounds(CanvasElement.ALL_PANES);
-		this.drawHighlightedBackgroundBetweenLabels(yAxisBounds);
+		this.drawHighlightedBackgroundBetweenLabels();
 		const orderedLabels = this.labelsProvider();
 		orderedLabels.forEach(l => {
 			const bounds = l.bounds ?? yAxisBounds;
@@ -68,42 +67,35 @@ export class YAxisPriceLabelsDrawer implements Drawer {
 
 	// this is a very simple solution which matches 2 labels with same "subGroupId"
 	// in future we may want to change it, but for now it's enough
-	drawHighlightedBackgroundBetweenLabels(bounds: Bounds) {
+	// and I guess it used only for drawings
+	drawHighlightedBackgroundBetweenLabels() {
 		const ctx = this.yAxisLabelsCanvasModel.ctx;
-		const acc: VisualYAxisLabel[] = [];
 		const orderedLabels = this.labelsProvider();
-		const flatLabels = orderedLabels.reduce((acc, labelGroup) => {
-			return acc.concat(labelGroup.labels);
-		}, acc);
-		const initial: { [key: string]: VisualYAxisLabel[] } = {};
-		const groupedLabels = flatLabels.reduce((acc, label) => {
-			if (label.subGroupId) {
-				let group = acc[label.subGroupId];
-				if (!group) {
-					group = [];
-					acc[label.subGroupId] = group;
+		const map: Record<string, VisualYAxisLabel[]> = {};
+		orderedLabels.forEach(group => {
+			group.labels.forEach(label => {
+				if (label.subGroupId) {
+					const labels = map[label.subGroupId] ?? [];
+					map[label.subGroupId] = labels;
+					labels.push(label);
+					if (labels.length === 2) {
+						const bounds =
+							group.bounds ??
+							this.canvasBoundsContainer.getBounds(CanvasElement.PANE_UUID_Y_AXIS(CHART_UUID));
+						ctx.save();
+						ctx.fillStyle = labels[0].highlightColor ?? labels[0].bgColor;
+						// start and end are sorted in ascending order
+						const [startY, endY] =
+							labels[0].y > labels[1].y ? [labels[1].y, labels[0].y] : [labels[0].y, labels[1].y];
+						fillRect(
+							ctx,
+							{ x: bounds.x, y: limitYToBounds(startY, bounds) },
+							{ x: bounds.x + bounds.width - 6, y: limitYToBounds(endY, bounds) },
+						);
+						ctx.restore();
+					}
 				}
-				group.push(label);
-			}
-			return acc;
-		}, initial);
-		Object.keys(groupedLabels).forEach(subGroupId => {
-			const labels = groupedLabels[subGroupId];
-			// expect 2 labels only
-			if (labels.length === 2) {
-				const yAxisBounds = this.canvasBoundsContainer.getBounds(CanvasElement.Y_AXIS);
-				ctx.save();
-				ctx.fillStyle = labels[0].highlightColor ?? labels[0].bgColor;
-				// start and end are sorted in ascending order
-				const [startY, endY] =
-					labels[0].y > labels[1].y ? [labels[1].y, labels[0].y] : [labels[0].y, labels[1].y];
-				fillRect(
-					ctx,
-					{ x: yAxisBounds.x, y: limitYToBounds(startY, bounds) },
-					{ x: yAxisBounds.x + yAxisBounds.width - 6, y: limitYToBounds(endY, bounds) },
-				);
-				ctx.restore();
-			}
+			});
 		});
 	}
 
