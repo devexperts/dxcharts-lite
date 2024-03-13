506c67d56503a6c67a0c229364edc90c5c94188d
Kirill Bobkov <kbobkov@devexperts.com>
2024-03-13 14:16:09 +0400

    fix: temporary removed tests to check Job process

--- src/chart/utils/__tests__/math.utils.test.ts
+++ src/chart/utils/__tests__/math.utils.test.ts
@@ -1,17 +1,17 @@
-import { clamp } from '../math.utils';
+// import { clamp } from '../math.utils';
+export default 5;
+// describe('math.utils', () => {
+	// describe('clamp', () => {
+	// 	it('should return the value if it is within the range', () => {
+	// 		expect(clamp(5, 0, 10)).toEqual(5);
+	// 	});
 
-describe('math.utils', () => {
-	describe('clamp', () => {
-		it('should return the value if it is within the range', () => {
-			expect(clamp(5, 0, 10)).toEqual(5);
-		});
+	// 	it('should return the min value if the value is less than the min', () => {
+	// 		expect(clamp(-5, 0, 10)).toEqual(0);
+	// 	});
 
-		it('should return the min value if the value is less than the min', () => {
-			expect(clamp(-5, 0, 10)).toEqual(0);
-		});
-
-		it('should return the max value if the value is more than the max', () => {
-			expect(clamp(15, 0, 10)).toEqual(10);
-		});
-	});
-});
+	// 	it('should return the max value if the value is more than the max', () => {
+	// 		expect(clamp(15, 0, 10)).toEqual(10);
+	// 	});
+	// });
+// });
