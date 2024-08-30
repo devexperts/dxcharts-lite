6989de1b942d130c34c30377d40e349961a61527
Denis Travin <denis.travin@devexperts.com>
2024-08-30 09:33:52 +0000

    Pull request #5275: Feature/DXCF-5463
    Merge in DXCHARTS/dxchart5 from feature/DXCF-5463 to master
    
    * commit 'd6272b239d505e648a78aca6396d0a46a4ab2b1f':
      DXCF-5463 // chore: updated package versions
      [DXCF-5463] [DXREQ-23641] Switching to using candle identifiers // copyright fix
      [DXCF-5463] [DXREQ-23641] Switching to using candle identifiers // init
    
    GitOrigin-RevId: 0cc40838765f22c1addf81e1d90c9521b86e040c

--- src/chart/drawers/drawing-manager.ts
+++ src/chart/drawers/drawing-manager.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 - 2024 Devexperts Solutions IE Limited
+ * Copyright (C) 2019 - 2023 Devexperts Solutions IE Limited
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
  * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
@@ -23,6 +23,7 @@ const drawerTypes = [
 	'OVER_SERIES_CLEAR',
 	'HIT_TEST_DRAWINGS',
 	'GRID',
+	'UNDERLAY_VOLUMES_AREA',
 	'X_AXIS',
 	'Y_AXIS',
 	'HIGH_LOW',
