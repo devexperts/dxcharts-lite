60858fc0b2b4762e6185309b6441be175742fb72
Denis Travin <denis.travin@devexperts.com>
2024-08-30 09:33:52 +0000

    Pull request #5275: Feature/DXCF-5463
    Merge in DXCHARTS/dxchart5 from feature/DXCF-5463 to master
    
    * commit 'd6272b239d505e648a78aca6396d0a46a4ab2b1f':
      DXCF-5463 // chore: updated package versions
      [DXCF-5463] [DXREQ-23641] Switching to using candle identifiers // copyright fix
      [DXCF-5463] [DXREQ-23641] Switching to using candle identifiers // init
    
    GitOrigin-RevId: 471c706479af428ff2058ad293f6ce54c6e9440f

--- src/chart/inputhandlers/hover-producer.component.ts
+++ src/chart/inputhandlers/hover-producer.component.ts
@@ -1,10 +1,9 @@
 /*
- * Copyright (C) 2019 - 2024 Devexperts Solutions IE Limited
+ * Copyright (C) 2019 - 2023 Devexperts Solutions IE Limited
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
  * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 import { BehaviorSubject, merge } from 'rxjs';
-import { filter, switchMap } from 'rxjs/operators';
 import { CHART_UUID, CanvasBoundsContainer, CanvasElement } from '../canvas/canvas-bounds-container';
 import { FullChartConfig } from '../chart.config';
 import { ChartModel } from '../components/chart/chart.model';
@@ -95,16 +94,11 @@ export class HoverProducerComponent extends ChartBaseElement {
 	protected doActivate() {
 		super.doActivate();
 		this.addRxSubscription(
-			// required for initial legend initialization, do not show cross tool
-			this.chartModel.candlesSetSubject
-				.pipe(
-					// check the scale is valid before doing candle-based hover event
-					switchMap(() => this.scale.initialViewportValidSubject.pipe(filter(Boolean))),
-				)
-				.subscribe(() => {
-					const lastCandle = this.chartModel.getLastVisualCandle();
-					lastCandle && this.createAndFireHoverFromCandle(lastCandle);
-				}),
+			this.chartModel.candlesSetSubject.subscribe(() => {
+				// required for initial legend initialization, do not show cross tool
+				const lastCandle = this.chartModel.getLastVisualCandle();
+				lastCandle && this.createAndFireHoverFromCandle(lastCandle);
+			}),
 		);
 		this.addRxSubscription(
 			this.chartModel.candlesUpdatedSubject.subscribe(() => {
