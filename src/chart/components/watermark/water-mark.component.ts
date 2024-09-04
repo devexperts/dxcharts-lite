34eebada265d111b9c3cbe49f04cbcd66be9489f
Anton Vorobev <avorobev@devexperts.com>
2024-09-04 10:01:07 +0000

    Pull request #5278: Feature/DXCF-5602 improve watermark behaviour for library
    Merge in DXCHARTS/dxchart5 from feature/DXCF-5602-improve-watermark-behaviour-for-library to master
    
    * commit '88ea0c3a22328a274bef532241ec2ef7b12dccaa':
      [DXCF-5602] Improve watermark behaviour for Library // pr fix
      [DXCF-5602] Improve watermark behaviour for Library // pr fix
      [DXCF-5602] Improve watermark behaviour for Library
      [DXCF-5602] Improve watermark behaviour for Library // init
      [DXCF-5602] Improve watermark behaviour for Library // revert
      [DXCF-5602] Improve watermark behaviour for Library // init
    
    GitOrigin-RevId: 31f42c018908d10644bfead6248dbed7e264a0a1

--- src/chart/components/watermark/water-mark.component.ts
+++ src/chart/components/watermark/water-mark.component.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 - 2023 Devexperts Solutions IE Limited
+ * Copyright (C) 2019 - 2024 Devexperts Solutions IE Limited
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
  * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
@@ -14,16 +14,6 @@ import { merge } from '../../utils/merge.utils';
 import { ChartModel } from '../chart/chart.model';
 import { PaneManager } from '../pane/pane-manager.component';
 import { WaterMarkDrawer } from './water-mark.drawer';
-
-export interface WaterMarkConfig {
-	isVisible?: boolean;
-	fontFamily?: string;
-	firstRowFontSize?: number;
-	firstRowBottomPadding?: number;
-	secondRowFontSize?: number;
-	secondRowBottomPadding?: number;
-	thirdRowFontSize?: number;
-}
 
 export interface WaterMarkData {
 	firstRow?: string;
@@ -103,23 +93,22 @@ export class WaterMarkComponent extends ChartBaseElement {
 
 	/**
      * Sets the watermark configuration for the chart.
-     * @param {WaterMarkConfig} watermarkConfig - The configuration object for the watermark.
+     * @param {ChartConfigComponentsWaterMark} watermarkConfig - The configuration object for the watermark.
      * @returns {void}
      
     */
-	public setWaterMarkConfig(watermarkConfig: WaterMarkConfig): void {
+	public setWaterMarkConfig(watermarkConfig: ChartConfigComponentsWaterMark): void {
 		if (!watermarkConfig || !this.config.components) {
 			return;
 		}
 		if (!this.config.components.waterMark) {
 			this.config.components.waterMark = JSON.parse(JSON.stringify(watermarkConfig));
 		} else {
-			const newWatermark: WaterMarkConfig = {};
+			const newWatermark: Partial<ChartConfigComponentsWaterMark> = {};
 			merge(newWatermark, watermarkConfig);
 			merge(newWatermark, this.config.components.waterMark);
 			// eslint-disable-next-line no-restricted-syntax
-			this.config.components.waterMark = newWatermark as ChartConfigComponentsWaterMark &
-				Required<ChartConfigComponentsWaterMark>;
+			this.config.components.waterMark = newWatermark as ChartConfigComponentsWaterMark;
 		}
 		this.canvasModel.fireDraw();
 	}
