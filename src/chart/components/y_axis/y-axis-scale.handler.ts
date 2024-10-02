77fb747fd4ecdc1872aa75884507015415a3ebdc
Denis Travin <denis.travin@devexperts.com>
2024-10-02 13:03:13 +0000

    Pull request #5295: Feature/DXREQ-23708
    Merge in DXCHARTS/dxchart5 from feature/DXREQ-23708 to master
    
    * commit '64e2ff74850998c5a77572225ee45ecc452716a6': (65 commits)
      DXCF-5640 // feat: added missing build scripts to demo dockerfile
      dxreq-23708 // fix: added bash to alpine linux image
      dxreq-23708 // chore: reordered packages in monorepo
      dxreq-23708 // feat: last updates to make sure everything works
      DXCF-5507 // feat: refactored mock events data provider for better readability
      DXCF-5507 // feat: updated IndicatorTemplatesProvider docs
      DXCF-5507 // feat: fixed wrong imports
      DXCF-5507 // chore: updated imports and code to support last changes
      DXCF-5507 // chore: updated package versions
      DXCF-5507 // chore: updated imports and code to support last changes
      DXCF-5507 // chore: updated package versions
      DXCF-5507 // feat: added info about +14days in trading sessions provider article
      DXCF-5507 // feat: added info about priceType and priceIncrements to mock chart data impl
      DXCF-5507 // feat: added docs about updateCallback
      DXCF-5507 // feat: added docs about updateCallback
      DXCF-5507 // feat: added API reference for each provider page
      DXCF-5507 // feat: small updates about providers
      DXCF-5507 // feat: added docs about trading sessions provider
      DXCF-5507 // feat: added basic info about events and news data providers
      DXCF-5507 // feat: added docs for a bunch of providers
      ...
    
    GitOrigin-RevId: 741efe624b04fe7f21877f8f9670ddcd70c690fc

--- src/chart/components/y_axis/y-axis-scale.handler.ts
+++ src/chart/components/y_axis/y-axis-scale.handler.ts
@@ -1,10 +1,15 @@
 /*
- * Copyright (C) 2019 - 2023 Devexperts Solutions IE Limited
+ * Copyright (C) 2019 - 2024 Devexperts Solutions IE Limited
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
  * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 import { Subject } from 'rxjs';
-import { CanvasBoundsContainer, CanvasElement, DEFAULT_MIN_PANE_HEIGHT, HitBoundsTest } from '../../canvas/canvas-bounds-container';
+import {
+	CanvasBoundsContainer,
+	CanvasElement,
+	DEFAULT_MIN_PANE_HEIGHT,
+	HitBoundsTest,
+} from '../../canvas/canvas-bounds-container';
 import { YAxisConfig } from '../../chart.config';
 import EventBus from '../../events/event-bus';
 import { ChartBaseElement } from '../../model/chart-base-element';
@@ -63,7 +68,8 @@ export class YAxisScaleHandler extends ChartBaseElement {
 				canvasInputListener,
 				panning,
 				{
-					dragPredicate: () => panning.chartAreaPanHandler.chartPanningOptions.vertical && config.type !== 'percent',
+					dragPredicate: () =>
+						panning.chartAreaPanHandler.chartPanningOptions.vertical && config.type !== 'percent',
 				},
 			);
 			this.addChildEntity(dragNDropYComponent);
@@ -148,7 +154,7 @@ export class YAxisScaleHandler extends ChartBaseElement {
 		this.hitTestCanvasModel.hitTestDrawersPredicateSubject.next(true);
 	};
 
-	public setDblTapCallback = (cb: () => void) => this.dblTapCallback = cb;
+	public setDblTapCallback = (cb: () => void) => (this.dblTapCallback = cb);
 
-	public setDblClickCallback = (cb: () => void) => this.dblClickCallback = cb;
+	public setDblClickCallback = (cb: () => void) => (this.dblClickCallback = cb);
 }
--- src/chart/utils/linkedList.utils.ts
+++ src/chart/utils/linkedList.utils.ts
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2019 - 2024 Devexperts Solutions IE Limited
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 export class ListNode<T> {
 	public data: T;
 	public next: ListNode<T> | null;
