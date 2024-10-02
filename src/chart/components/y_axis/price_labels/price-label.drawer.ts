a14bbaacf3166fe7e43b7428848b472735638903
Denis Travin <denis.travin@devexperts.com>
2024-10-02 13:03:13 +0000

    Pull request #5295: Feature/DXREQ-23708
    Merge in DXCHARTS/dxchart5 from feature/DXREQ-23708 to master
    
    * commit '64e2ff74850998c5a77572225ee45ecc452716a6': (65 commits)
      DXCF-5640 // feat: added missing build scripts to demo dockerfile
      dxreq-23708 // fix: added bash to alpine linux image
      dxreq-23708 // chore: reordered packages in monorepo
      dxreq-23708 // feat: last updates to make sure everything works
      DXCF-5507 // feat: refactored mock events data provider for better readability
      DXCF-5507 // feat: updated IndicatorTemplatesProvider docs
      DXCF-5507 // feat: fixed wrong imports
      DXCF-5507 // chore: updated imports and code to support last changes
      DXCF-5507 // chore: updated package versions
      DXCF-5507 // chore: updated imports and code to support last changes
      DXCF-5507 // chore: updated package versions
      DXCF-5507 // feat: added info about +14days in trading sessions provider article
      DXCF-5507 // feat: added info about priceType and priceIncrements to mock chart data impl
      DXCF-5507 // feat: added docs about updateCallback
      DXCF-5507 // feat: added docs about updateCallback
      DXCF-5507 // feat: added API reference for each provider page
      DXCF-5507 // feat: small updates about providers
      DXCF-5507 // feat: added docs about trading sessions provider
      DXCF-5507 // feat: added basic info about events and news data providers
      DXCF-5507 // feat: added docs for a bunch of providers
      ...
    
    GitOrigin-RevId: 89c1caa26332bc7c1c2171e7aa15c1914bac46b5

--- src/chart/components/y_axis/price_labels/price-label.drawer.ts
+++ src/chart/components/y_axis/price_labels/price-label.drawer.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 - 2023 Devexperts Solutions IE Limited
+ * Copyright (C) 2019 - 2024 Devexperts Solutions IE Limited
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
  * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
@@ -77,7 +77,7 @@ export function drawLabel(
 	const showLine = isLineVisible(bounds, labelY, labelBoxHeight);
 
 	const _drawDescription = () =>
-	showDescription && drawDescription(backgroundCtx, ctx, bounds, paneBounds, visualLabel, config);
+		showDescription && drawDescription(backgroundCtx, ctx, bounds, paneBounds, visualLabel, config);
 
 	let lineXStart: number;
 	let lineXEnd: number;
