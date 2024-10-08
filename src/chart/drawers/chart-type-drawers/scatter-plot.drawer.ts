b3b08ef438918823223d0d8ac7ef453951156070
Pavel Malyshev <pmalyshev@devexperts.com>
2024-10-08 20:10:11 +0300

    feat: try to avoid removing github workflows during copybara sync process
    GitOrigin-RevId: f588d2f8711098b1fb7625bbdf460ba31117c88c

--- src/chart/drawers/chart-type-drawers/scatter-plot.drawer.ts
+++ src/chart/drawers/chart-type-drawers/scatter-plot.drawer.ts
@@ -1,12 +1,13 @@
 /*
- * Copyright (C) 2019 - 2025 Devexperts Solutions IE Limited
+ * Copyright (C) 2019 - 2024 Devexperts Solutions IE Limited
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
  * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 import { ScatterPlotStyle } from '../../chart.config';
 import { DataSeriesModel, VisualSeriesPoint } from '../../model/data-series.model';
 import { flat } from '../../utils/array.utils';
-import { HTSeriesDrawerConfig, SeriesDrawer } from '../data-series.drawer';
+import { floorToDPR } from '../../utils/device/device-pixel-ratio.utils';
+import { ChartDrawerConfig, SeriesDrawer } from '../data-series.drawer';
 
 const SCATTER_PLOT_RADIUS = 1.5;
 
@@ -17,16 +18,14 @@ export class ScatterPlotDrawer implements SeriesDrawer {
 		ctx: CanvasRenderingContext2D,
 		points: VisualSeriesPoint[][],
 		model: DataSeriesModel,
-		hitTestDrawerConfig: HTSeriesDrawerConfig,
+		drawerConfig: ChartDrawerConfig,
 	) {
-		const radius = hitTestDrawerConfig.hoverWidth ? hitTestDrawerConfig.hoverWidth / 2 : SCATTER_PLOT_RADIUS;
-
-		ctx.fillStyle = hitTestDrawerConfig.color ?? this.config.mainColor;
+		ctx.fillStyle = drawerConfig.singleColor ?? this.config.mainColor;
 		for (const visualCandle of flat(points)) {
 			ctx.beginPath();
-			const lineX = model.view.toX(visualCandle.centerUnit);
-			const closeY = model.view.toY(visualCandle.close);
-			ctx.arc(lineX, closeY, radius, 0, Math.PI * 2, true);
+			const lineX = floorToDPR(model.view.toX(visualCandle.centerUnit));
+			const closeY = floorToDPR(model.view.toY(visualCandle.close));
+			ctx.arc(lineX, closeY, SCATTER_PLOT_RADIUS, 0, Math.PI * 2, true);
 			ctx.fill();
 		}
 	}
