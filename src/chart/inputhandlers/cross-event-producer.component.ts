2e1874d11d23089b857a41d6ffb9065fde3f912b
Anton Vorobev <avorobev@devexperts.com>
2024-10-10 13:15:16 +0000

    Pull request #5312: Feature/DXCF-5637 adaptive web crosshair behaviour adjustments
    Merge in DXCHARTS/dxchart5 from feature/DXCF-5637-adaptive-web-crosshair-behaviour-adjustments to master
    
    * commit 'f11d7e0a1e475ad90dded22d139e42bdb835117b':
      [DXCF-5637] [Adaptive web] Crosshair behaviour adjustments // pr fix
      [DXCF-5637] [Adaptive web] Crosshair behaviour adjustments // remove copyrights
      [DXCF-5637] [Adaptive web] Crosshair behaviour adjustments // pr fix
      [DXCF-5637] [Adaptive web] Crosshair behaviour adjustments // pr fix
      [DXCF-5637] [Adaptive web] Crosshair behaviour adjustments // pr fix
      [DXCF-5637] [Adaptive web] Crosshair behaviour adjustments // init
      [DXCF-5637] [Adaptive web] Crosshair behaviour adjustments // init
      [DXCF-5637] [Adaptive web] Crosshair behaviour adjustments // init
      [DXCF-5637] [Adaptive web] Crosshair behaviour adjustments // init
      [DXCF-5637] [Adaptive web] Crosshair behaviour adjustments // init
      [DXCF-5637] [Adaptive web] Crosshair behaviour adjustments // init
      [DXCF-5637] [Adaptive web] Crosshair behaviour adjustments // init
      [DXCF-5637] [Adaptive web] Crosshair behaviour adjustments // init
      [DXCF-5637] [Adaptive web] Crosshair behaviour adjustments // init
      [DXCF-5637] [Adaptive web] Crosshair behaviour adjustments // init
      [DXCF-5637] [Adaptive web] Crosshair behaviour adjustments // init
      [DXCF-5637] [Adaptive web] Crosshair behaviour adjustments // init
      [DXCF-5637] [Adaptive web] Crosshair behaviour adjustments // init
      [DXCF-5637] [Adaptive web] Crosshair behaviour adjustments // init
      [DXCF-5637] [Adaptive web] Crosshair behaviour adjustments // init
    
    GitOrigin-RevId: f865277b63663fe3754bb611662f74e026e2a715

--- src/chart/inputhandlers/cross-event-producer.component.ts
+++ src/chart/inputhandlers/cross-event-producer.component.ts
@@ -3,20 +3,58 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
  * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
+/*
+ * Copyright (C) 2019 - 2024 Devexperts Solutions IE Limited
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 import { BehaviorSubject, Subscription, combineLatest } from 'rxjs';
 import { filter } from 'rxjs/operators';
 import { CanvasBoundsContainer, HitBoundsTest, HitBoundsTestOptionsPartial } from '../canvas/canvas-bounds-container';
 import { CanvasInputListenerComponent } from '../inputlisteners/canvas-input-listener.component';
 import { ChartBaseElement } from '../model/chart-base-element';
 import { Unsubscriber } from '../utils/function.utils';
+import { Pixel } from '../model/scaling/viewport.model';
+import { Hover } from './hover-producer.component';
+import { isMobile } from '../utils/device/browser.utils';
 
 /**
  * [x, y, uuid - Unique identifier for the subscription]
  */
 export type CrossEvent = [number, number, string];
+
+interface CrossToolTouchInfo {
+	// the placement of crosstool when it's set after long touch and further moving happens
+	fixed: {
+		x: Pixel;
+		y: Pixel;
+	};
+	// // the placement of crosstool after touchMove and touchEnd happened
+	temp: {
+		x: Pixel;
+		y: Pixel;
+	};
+	// true after longTouch event, initial movement and touchEnd happen
+	isSet: boolean;
+	/**
+	 * additional flag to determine ordinary and long taps
+	 * crosstool shouldn't be hidden after longtouch event even if coordinates are the same
+	 * becomes true after touchStart and false after longTouchStart
+	 */
+	isCommonTap: boolean;
+}
+
 export class CrossEventProducerComponent extends ChartBaseElement {
 	panesSubscriptions: Partial<Record<string, Subscription>> = {};
 	public crossSubject: BehaviorSubject<CrossEvent | null> = new BehaviorSubject<CrossEvent | null>(null);
+	// mobile specific crosstool hover and touch info
+	public crossToolHover: Hover | null = null;
+	public crossToolTouchInfo: CrossToolTouchInfo = {
+		fixed: { x: 0, y: 0 },
+		temp: { x: 0, y: 0 },
+		isSet: false,
+		isCommonTap: false,
+	};
 	constructor(
 		private canvasInputListener: CanvasInputListenerComponent,
 		private canvasBoundsContainer: CanvasBoundsContainer,
@@ -80,7 +118,9 @@ export class CrossEventProducerComponent extends ChartBaseElement {
 					const cross: CrossEvent = [point.x, point.y, uuid];
 					this.crossSubject.next(cross);
 					closeHoverFired = false;
-				} else {
+				}
+				// crosstool should be hidden if hovering nonpane only on desktop
+				if (!enter && !isMobile()) {
 					this.crossSubject.next(null);
 					closeHoverFired = true;
 				}
