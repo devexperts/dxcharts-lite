305a054c7099555f7455e237da5b406a6b34dbec
Anton Vorobev <avorobev@devexperts.com>
2024-10-25 09:03:50 +0000

    Pull request #5332: Bugfix/DXCF-5668 tdw missing localization
    Merge in DXCHARTS/dxchart5 from bugfix/DXCF-5668-tdw-missing-localization to master
    
    * commit 'b318dda6d9daa5c3e166b6c35226598737084c75':
      [DXCF-5668] TDW - Missing localization // pr fix
      [DXCF-5668] TDW - Missing localization // pr fix
      [DXCF-5668] TDW - Missing localization // pr fix
      [DXCF-5668] TDW - Missing localization // pr fix
      [DXCF-5668] TDW - Missing localization // pr fix
      [DXCF-5668] TDW - Missing localization // pr fix
      [DXCF-5668] TDW - Missing localization // init
      [DXCF-5668] TDW - Missing localization // init
      [DXCF-5668] TDW - Missing localization // init
      [DXCF-5668] TDW - Missing localization // init
    
    GitOrigin-RevId: 4e33e34cbec3b47e5f033acc2302a8c46a246201

--- src/chart/components/high_low/high-low.drawer.ts
+++ src/chart/components/high_low/high-low.drawer.ts
@@ -3,6 +3,11 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
  * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
+/*
+ * Copyright (C) 2019 - 2024 Devexperts Solutions IE Limited
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 import { Drawer } from '../../drawers/drawing-manager';
 import { CanvasBoundsContainer, CanvasElement } from '../../canvas/canvas-bounds-container';
 import { FullChartConfig } from '../../chart.config';
@@ -84,7 +89,8 @@ export class HighLowDrawer implements Drawer {
 	 */
 	private getMarkerText(yValue: number, type: MarkerType): string {
 		const formattedValue = this.chartModel.pane.regularFormatter(yValue);
-		const prefix = type === 'high' ? 'H:' : 'L:';
+		const prefix =
+			type === 'high' ? this.config.components.highLow.prefix.high : this.config.components.highLow.prefix.low;
 		return `${prefix} ${formattedValue}`;
 	}
 
