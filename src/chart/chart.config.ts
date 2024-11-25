0fd3cb757f8b4838d923e3f64fa1d2f0e6dc67b5
Anton Vorobev <avorobev@devexperts.com>
2024-11-25 13:39:37 +0000

    Pull request #5354: Feature/DXCF-5565 web multiple scales implement external supported api to manipulate multiple scales
    Merge in DXCHARTS/dxchart5 from feature/DXCF-5565-web-multiple-scales-implement-external-supported-api-to-manipulate-multiple-scales to master
    
    * commit '00a3db32c607b22be8570a0ea5822b622d21f7e8':
      [DXCF-5565] [Web] Multiple scales - implement external (supported) API to manipulate multiple scales from user-land code // pr fix
      [DXCF-5565] [Web] Multiple scales - implement external (supported) API to manipulate multiple scales from user-land code // pr fix
      [DXCF-5565] [Web] Multiple scales - implement external (supported) API to manipulate multiple scales from user-land code // init
    
    GitOrigin-RevId: cf2847c9f99627acd99bb12da67fa6cedf7375a4

--- src/chart/chart.config.ts
+++ src/chart/chart.config.ts
@@ -3,6 +3,16 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
  * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
+/*
+ * Copyright (C) 2019 - 2024 Devexperts Solutions IE Limited
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+/*
+ * Copyright (C) 2019 - 2024 Devexperts Solutions IE Limited
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 import { PriceAxisType } from './components/labels_generator/numeric-axis-labels.generator';
 import { MagnetTarget } from './components/cross_tool/cross-tool.component';
 import { CrossToolType } from './components/cross_tool/cross-tool.model';
