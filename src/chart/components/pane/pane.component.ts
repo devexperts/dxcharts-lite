d9b5998852ffffd18704332ec3ccdc9b06d06721
Anton Vorobev <avorobev@devexperts.com>
2024-11-25 13:39:37 +0000

    Pull request #5354: Feature/DXCF-5565 web multiple scales implement external supported api to manipulate multiple scales
    Merge in DXCHARTS/dxchart5 from feature/DXCF-5565-web-multiple-scales-implement-external-supported-api-to-manipulate-multiple-scales to master
    
    * commit '00a3db32c607b22be8570a0ea5822b622d21f7e8':
      [DXCF-5565] [Web] Multiple scales - implement external (supported) API to manipulate multiple scales from user-land code // pr fix
      [DXCF-5565] [Web] Multiple scales - implement external (supported) API to manipulate multiple scales from user-land code // pr fix
      [DXCF-5565] [Web] Multiple scales - implement external (supported) API to manipulate multiple scales from user-land code // init
    
    GitOrigin-RevId: 4098ddf0afe140453009542d178cddf4a6af8b88

--- src/chart/components/pane/pane.component.ts
+++ src/chart/components/pane/pane.component.ts
@@ -3,6 +3,11 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
  * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
+/*
+ * Copyright (C) 2019 - 2024 Devexperts Solutions IE Limited
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 import { Subject } from 'rxjs';
 import { distinctUntilChanged } from 'rxjs/operators';
 import { CanvasAnimation } from '../../animation/canvas-animation';
