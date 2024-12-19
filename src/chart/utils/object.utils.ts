5c06acfcb15c078d56056f78a1035ef40c261503
Anton Vorobev <avorobev@devexperts.com>
2024-12-19 13:55:20 +0000

    Pull request #5387: Feature/DXCF-5643 web multiple scales add layout support
    Merge in DXCHARTS/dxchart5 from feature/DXCF-5643-web-multiple-scales-add-layout-support to master
    
    * commit '2f5c22e5170a7b600e78a7fffb0cda11aadc5bfb':
      [DXCF-5643] [Web] Multiple scales - add layout support // pr fix
      [DXCF-5643] [Web] Multiple scales - add layout support // pr fix
      [DXCF-5643] [Web] Multiple scales - add layout support // reverse order entry config
      [DXCF-5643] [Web] Multiple scales - add layout support // fix scales set from layout
      [DXCF-5643] [Web] Multiple scales - add layout support // init
      [DXCF-5643] [Web] Multiple scales - add layout support // fix merge check
      [DXCF-5643] [Web] Multiple scales - add layout support // init
      [DXCF-5643] [Web] Multiple scales - add layout support // init
      [DXCF-5643] [Web] Multiple scales - add layout support // init
      [DXCF-5643] [Web] Multiple scales - add layout support // init
      [DXCF-5643] [Web] Multiple scales - add layout support // init
      [DXCF-5643] [Web] Multiple scales - add layout support // init
      [DXCF-5643] [Web] Multiple scales - add layout support // init
      [DXCF-5643] [Web] Multiple scales - add layout support // init
      [DXCF-5643] [Web] Multiple scales - add layout support // init
      [DXCF-5643] [Web] Multiple scales - add layout support // init
      [DXCF-5643] [Web] Multiple scales - add layout support // init
    
    GitOrigin-RevId: c09ee1a6d9d1eed1997529a0db3e50e55781f0d0

--- src/chart/utils/object.utils.ts
+++ src/chart/utils/object.utils.ts
@@ -3,6 +3,11 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
  * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
+/*
+ * Copyright (C) 2019 - 2024 Devexperts Solutions IE Limited
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 /**
  * @description
  * Helps with the partial objects functions arguments, but errors the empty object case, which is allowed by simple Partial<T>
@@ -95,3 +100,5 @@ export function deepEqual(objA: object, objB: object): boolean {
 
 	return true;
 }
+
+export const isEmpty = (obj: object) => Object.keys(obj).length === 0 && obj.constructor === Object;
