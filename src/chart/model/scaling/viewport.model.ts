014f89f4e76cf4506b7298f945a2e400a08e5953
Anton Vorobev <avorobev@devexperts.com>
2024-12-24 15:06:24 +0000

    Pull request #5412: Bugfix/DXCF-5765 chart viewport incorrect viewport position after reload page
    Merge in DXCHARTS/dxchart5 from bugfix/DXCF-5765-chart-viewport-incorrect-viewport-position-after-reload-page to master
    
    * commit '4f664c0a3126d3692ec4df8751e2747d6a566a96':
      [DXCF-5765] Chart Viewport - Incorrect viewport position after reload page // copy
      [DXCF-5765] Chart Viewport - Incorrect viewport position after reload page // init
      [DXCF-5765] Chart Viewport - Incorrect viewport position after reload page // init
      [DXCF-5765] Chart Viewport - Incorrect viewport position after reload page // init
      [DXCF-5765] Chart Viewport - Incorrect viewport position after reload page // init
    
    GitOrigin-RevId: 4e59b3010ca54404b2a878ea52d8464ab3deb7ef

--- src/chart/model/scaling/viewport.model.ts
+++ src/chart/model/scaling/viewport.model.ts
@@ -424,14 +424,15 @@ export abstract class ViewportModel extends ChartBaseElement implements Viewable
 	 *
 	 * @returns {boolean} - Returns true if the viewport is valid, false otherwise.
 	 */
-	isViewportValid() {
+	isViewportValid(validateZoom: boolean = true) {
+		// zoom is checked separately because sometimes we need to recalculate zoom based on X and Y and zoom could be incorrect
+		const isZoomValid = validateZoom === false || (this.zoomX > 0 && this.zoomY > 0);
 		return (
 			this.xStart !== this.xEnd &&
 			this.yStart !== this.yEnd &&
 			isFinite(this.yStart) &&
 			isFinite(this.yEnd) &&
-			this.zoomX > 0 &&
-			this.zoomY > 0
+			isZoomValid
 		);
 	}
 }
