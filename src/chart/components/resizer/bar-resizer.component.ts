56567feeab48d86b20b42c8fb4d4675ba41199b5
Anton Vorobev <avorobev@devexperts.com>
2025-01-13 09:27:49 +0000

    Pull request #5400: Feature/DXCF-5569 web multiple scales syncing for multi chart layouts
    Merge in DXCHARTS/dxchart5 from feature/DXCF-5569-web-multiple-scales-syncing-for-multi-chart-layouts to master
    
    * commit 'aca4c582a6b2d70025451deda1442114909e9102':
      [DXCF-5569] [Web] Multiple scales - syncing for multi-chart layouts // pr fix
      [DXCF-5569] [Web] Multiple scales - syncing for multi-chart layouts // fix standard deviation channel default overlaying value
      [DXCF-5569] [Web] Multiple scales - syncing for multi-chart layouts // pr fix
      [DXCF-5569] [Web] Multiple scales - syncing for multi-chart layouts // pr fix
      [DXCF-5569] [Web] Multiple scales - syncing for multi-chart layouts // pr fix
      [DXCF-5569] [Web] Multiple scales - syncing for multi-chart layouts // copy
      [DXCF-5569] [Web] Multiple scales - syncing for multi-chart layouts // init
      [DXCF-5569] [Web] Multiple scales - syncing for multi-chart layouts // init
      [DXCF-5569] [Web] Multiple scales - syncing for multi-chart layouts // init
      [DXCF-5569] [Web] Multiple scales - syncing for multi-chart layouts // init
      [DXCF-5569] [Web] Multiple scales - syncing for multi-chart layouts // init
      [DXCF-5569] [Web] Multiple scales - syncing for multi-chart layouts // init
      [DXCF-5569] [Web] Multiple scales - syncing for multi-chart layouts // init
      [DXCF-5569] [Web] Multiple scales - syncing for multi-chart layouts // init
      [DXCF-5569] [Web] Multiple scales - syncing for multi-chart layouts // init
      [DXCF-5569] [Web] Multiple scales - syncing for multi-chart layouts // init
    
    GitOrigin-RevId: 649fc4df1c9a13135158571b8c42fcd2b650290a

--- src/chart/components/resizer/bar-resizer.component.ts
+++ src/chart/components/resizer/bar-resizer.component.ts
@@ -1,4 +1,9 @@
 /*
+ * Copyright (C) 2019 - 2025 Devexperts Solutions IE Limited
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+/*
  * Copyright (C) 2019 - 2024 Devexperts Solutions IE Limited
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
  * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
