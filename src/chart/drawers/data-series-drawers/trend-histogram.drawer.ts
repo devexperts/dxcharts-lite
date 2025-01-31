312c81d749ae9c3f0ae7921f2a1598e734889b58
Anton Vorobev <avorobev@devexperts.com>
2025-01-31 09:04:35 +0000

    Pull request #5446: Bugfix/DXCF-5792 indicators values for indicators in the legend are displayed incorrectly when
    Merge in DXCHARTS/dxchart5 from bugfix/DXCF-5792-indicators-values-for-indicators-in-the-legend-are-displayed-incorrectly-when to master
    
    * commit '36c003f3e91eef3dcc355afa9f8cac1cdbe9ddda':
      [DXCF-5792] Indicators - Values for indicators in the legend are displayed incorrectly when several indicators are added // init
      [DXCF-5792] Indicators - Values for indicators in the legend are displayed incorrectly when several indicators are added // fix docs lite examples viewport
      [DXCF-5792] Indicators - Values for indicators in the legend are displayed incorrectly when several indicators are added // init
      [DXCF-5792] Indicators - Values for indicators in the legend are displayed incorrectly when several indicators are added // init
    
    GitOrigin-RevId: f22fd45dc16a3674f6dd4a3ed7472d95653caa78

--- src/chart/drawers/data-series-drawers/trend-histogram.drawer.ts
+++ src/chart/drawers/data-series-drawers/trend-histogram.drawer.ts
@@ -3,7 +3,6 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
  * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
-
 import { VisualSeriesPoint, DataSeriesModel } from '../../model/data-series.model';
 import { flat } from '../../utils/array.utils';
 import { SeriesDrawer } from '../data-series.drawer';
