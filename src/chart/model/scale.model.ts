8ad8a6e7977ea9bf2f7367e296917d81568e528a
Peter Panayotov <ppanayotov@devexperts.com>
2025-02-07 14:01:49 +0000

    Pull request #5475: [DXCF-5840] Chart animations improvement - fix
    Merge in DXCHARTS/dxchart5 from feature/DXCF-5840-chart-animations-improvement to master
    
    * commit '8e109a557a0e0e5db8bfa2c013a690e492911ebe':
      [DXCF-5840] Chart animations improvement - fix
      [DXCF-5840] Chart animations improvement - fix
      [DXCF-5840] Chart animations improvement - fix
      [DXCF-5840] Chart animations improvement - fix
    
    GitOrigin-RevId: 62712f93e1f9d214240ee25bbc2229b6ef8ee436

--- src/chart/model/scale.model.ts
+++ src/chart/model/scale.model.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 - 2024 Devexperts Solutions IE Limited
+ * Copyright (C) 2019 - 2025 Devexperts Solutions IE Limited
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
  * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
@@ -198,6 +198,7 @@ export class ScaleModel extends ViewportModel {
 		if (forceNoAnimation) {
 			this.apply(constrainedState);
 		} else {
+			this.currentAnimation?.tick();
 			startViewportModelAnimation(this.canvasAnimation, this, constrainedState);
 		}
 	}
@@ -255,6 +256,7 @@ export class ScaleModel extends ViewportModel {
 			this.haltAnimation();
 			this.apply(constrainedState);
 		} else {
+			this.currentAnimation?.tick();
 			startViewportModelAnimation(this.canvasAnimation, this, constrainedState);
 		}
 	}
