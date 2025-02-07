3338b76b5cacab7817bb9d313d508663afd78c87
Peter Panayotov <ppanayotov@devexperts.com>
2025-02-07 14:01:49 +0000

    Pull request #5475: [DXCF-5840] Chart animations improvement - fix
    Merge in DXCHARTS/dxchart5 from feature/DXCF-5840-chart-animations-improvement to master
    
    * commit '8e109a557a0e0e5db8bfa2c013a690e492911ebe':
      [DXCF-5840] Chart animations improvement - fix
      [DXCF-5840] Chart animations improvement - fix
      [DXCF-5840] Chart animations improvement - fix
      [DXCF-5840] Chart animations improvement - fix
    
    GitOrigin-RevId: ace466673694a38ea6e35eb9b04034e41fce9180

--- src/chart/animation/types/animation.ts
+++ src/chart/animation/types/animation.ts
@@ -1,57 +1,54 @@
 /*
- * Copyright (C) 2019 - 2024 Devexperts Solutions IE Limited
+ * Copyright (C) 2019 - 2025 Devexperts Solutions IE Limited
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
  * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 import { AnimationConfig } from '../canvas-animation';
 
-interface AnimationAware {
-	tick: () => void;
-}
+type OnTickFunction = () => void;
 
 /**
  * Basic animation. Holds current state: inProgress, timeLeft.
  * Additional params are useful to pass data which helps animating.
  */
-export class Animation implements AnimationAware {
+export class Animation {
 	animationTime: number;
 	animationTimeLeft = 0;
-	animationInProgress = false;
+	animationStartTime: number;
+	animationInProgress: boolean;
+	onTickFunction?: OnTickFunction;
 
-	constructor(animationConfig: AnimationConfig, private onTickFunction?: () => void) {
+	constructor(animationConfig: AnimationConfig, onTickFunction: OnTickFunction) {
+		this.onTickFunction = onTickFunction;
 		this.animationTime = animationConfig.duration;
-		this.animationTimeLeft =
-			animationConfig.timeLeft === undefined ? animationConfig.duration : animationConfig.timeLeft;
+		this.animationStartTime = Date.now();
 		this.animationInProgress = true;
 	}
 
-	/**
+	/*
 	 * Decrements the animation time left and executes the onTickFunction if it exists.
 	 * If the animation time left reaches zero, sets the animationInProgress flag to false.
 	 */
 	tick() {
-		if (this.animationTimeLeft <= 0) {
+		if (Date.now() > this.animationStartTime + this.animationTime) {
 			this.animationInProgress = false;
-		} else {
-			this.animationTimeLeft--;
 		}
-		this.onTickFunction && this.onTickFunction();
+		this.onTickFunction?.();
 	}
 
-	// normalized time [0..1]
-	/**
-	 * Calculates the normalized progress of an animation.
+	/*
+	 * Gets the normalized progress of the animation.
 	 * @returns {number} A value between 0 and 1 representing the progress of the animation.
 	 */
 	getProgress() {
-		return 1 - this.animationTimeLeft / this.animationTime;
+		return Math.min((Date.now() - this.animationStartTime) / this.animationTime, 1);
 	}
 
-	/**
-	 * Sets the animationInProgress flag to false and resets the animationTimeLeft to 0.
+	/*
+	 * Sets the animationInProgress flag to false and resets the animationStartTime to 0.
 	 */
-	public finishAnimation() {
+	finishAnimation() {
 		this.animationInProgress = false;
-		this.animationTimeLeft = 0;
+		this.animationStartTime = 0;
 	}
 }
