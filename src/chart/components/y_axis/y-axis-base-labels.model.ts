5e17554eddc38114fab6e18a72c76c16399e1fff
Anton Vorobev <avorobev@devexperts.com>
2025-03-11 13:33:02 +0000

    Pull request #5568: Bugfix/DXCF-5962 web multiple scales data formatting changes after move to pane
    Merge in DXCHARTS/dxchart5 from bugfix/DXCF-5962-web-multiple-scales-data-formatting-changes-after-move-to-pane to master
    
    * commit '2047c7d80a3f3efcbb181a1414f924b28260254c':
      CI: update snapshots
      [DXCF-5962] [Web] Multiple scales - Data formatting changes after move to pane // init
      [DXCF-5962] [Web] Multiple scales - Data formatting changes after move to pane // init
      [DXCF-5962] [Web] Multiple scales - Data formatting changes after move to pane // init
    
    GitOrigin-RevId: aa0de26fa17e78684b87d023d6a323abf51d4a5e

--- src/chart/components/y_axis/y-axis-base-labels.model.ts
+++ src/chart/components/y_axis/y-axis-base-labels.model.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 - 2024 Devexperts Solutions IE Limited
+ * Copyright (C) 2019 - 2025 Devexperts Solutions IE Limited
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
  * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
@@ -54,6 +54,16 @@ export class YAxisBaseLabelsModel extends ChartBaseElement {
 					),
 			).subscribe(() => this.updateLabels()),
 		);
+	}
+
+	/**
+	 * Generates new labels with labelsGenerator
+	 * Then, it calls the updateYAxisWidth method to update the width of the y-axis.
+	 */
+	public recalculateLabels() {
+		const labels = this.labelsGenerator.doGenerateLabels();
+		this.labels = labels;
+		animationFrameThrottledPrior(this.animFrameId, () => this.canvasBoundsContainer.updateYAxisWidths());
 	}
 
 	/**
