bbc1265caba65f25c5ddb2f63ede5569d29590f7
Anton Vorobev <avorobev@devexperts.com>
2025-05-27 09:24:38 +0000

    Pull request #5715: Feature/DXCF-6032 additional configuration for viewport behavior
    Merge in DXCHARTS/dxchart5 from feature/DXCF-6032-additional-configuration-for-viewport-behavior to master
    
    Squashed commit of the following:
    
    commit efee1f7ae78c525fd17559622d8c37dc5aa01569
    Merge: 8793e8f26e f72ff9c868
    Author: Anton Vorobev <avorobev@devexperts.com>
    Date:   Tue May 27 11:29:24 2025 +0300
    
        Merge branch 'master' of ssh://stash.in.devexperts.com:7999/dxcharts/dxchart5 into feature/DXCF-6032-additional-configuration-for-viewport-behavior
    
    commit 8793e8f26ec49162440b8af310c3ee69e0d59765
    Author: Anton Vorobev <avorobev@devexperts.com>
    Date:   Mon May 19 16:26:22 2025 +0300
    
        [DXCF-6032] Additional configuration for viewport behavior // pr fixes
    
    commit 0884641e9d28749a85dc3c09f627858fec946770
    Author: dxcity <dxcity@bots.devexperts.com>
    Date:   Fri May 16 10:32:57 2025 +0000
    
        CI: update snapshots
    
    commit 6b6e5d5e55fae5af0195ad221cbccb97e29ed432
    Author: dxcity <dxcity@bots.devexperts.com>
    Date:   Fri May 16 09:10:57 2025 +0000
    
        CI: update snapshots
    
    commit 76d91d8dccd73d910f458bac3e652eafc131aeef
    Merge: b7fa0ae67f b6fd5b087f
    Author: Anton Vorobev <avorobev@devexperts.com>
    Date:   Fri May 16 12:06:40 2025 +0300
    
        Merge branch 'master' of ssh://stash.in.devexperts.com:7999/dxcharts/dxchart5 into feature/DXCF-6032-additional-configuration-for-viewport-behavior
    
    commit b7fa0ae67f351335d0f94c0e210965e6c19325bc
    Author: Anton Vorobev <avorobev@devexperts.com>
    Date:   Fri May 16 12:06:28 2025 +0300
    
        Revert "CI: update snapshots"
    
        This reverts commit e586daca995d079aee48b4a7117ae3c294794d2e.
    
    commit e586daca995d079aee48b4a7117ae3c294794d2e
    Author: dxcity <dxcity@bots.devexperts.com>
    Date:   Fri May 16 08:56:16 2025 +0000
    
        CI: update snapshots
    
    commit 061042b5a6b8d472e7671a3b1cde743d67660aea
    Author: Anton Vorobev <avorobev@devexperts.com>
    Date:   Thu May 15 17:31:45 2025 +0300
    
        [DXCF-6032] Additional configuration for viewport behavior // init
    
    commit 8dd867a268dc959284ff191957019c2e5b793d34
    Author: Anton Vorobev <avorobev@devexperts.com>
    Date:   Thu May 15 16:37:04 2025 +0300
    
        [DXCF-6032] Additional configuration for viewport behavior // init
    
    commit 8badf24508d9fe05effd008f8f1d8b30c242f0b3
    Author: Anton Vorobev <avorobev@devexperts.com>
    Date:   Thu May 15 15:47:57 2025 +0300
    
        [DXCF-6032] Additional configuration for viewport behavior // init
    
    commit 5df8ff1cbbfbbad5c2198867f5f683cc57e7b800
    Author: Anton Vorobev <avorobev@devexperts.com>
    Date:   Thu May 15 15:23:01 2025 +0300
    
        [DXCF-6032] Additional configuration for viewport behavior // init
    
    commit 49d9c949dc754ccf173cac0ba944cef741b534b5
    Author: Anton Vorobev <avorobev@devexperts.com>
    Date:   Thu May 15 14:48:23 2025 +0300
    
        [DXCF-6032] Additional configuration for viewport behavior // init
    
    GitOrigin-RevId: 80156d9afeb090d9b2debfa61914d3e257ade544

--- src/chart/model/data-series.model.ts
+++ src/chart/model/data-series.model.ts
@@ -3,6 +3,11 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
  * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
+/*
+ * Copyright (C) 2019 - 2025 Devexperts Solutions IE Limited
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 import { YExtentComponent } from '../components/pane/extent/y-extent-component';
 import { DataSeriesYAxisLabelsProvider } from '../components/y_axis/price_labels/data-series-y-axis-labels.provider';
 import { LabelsGroups } from '../components/y_axis/price_labels/y-axis-labels.model';
