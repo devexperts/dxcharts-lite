0e1dc22559823915405d17d5280512d6743f6529
Vanina Kalapsazova <vkalapsazova@devexperts.com>
2025-10-27 08:58:25 +0000

    Pull request #6053: [DXCF-5832] [The5ers][Web] Modal size of the settings menu remains full screen after resizing the browser back to desktop size.
    Merge in DXCHARTS/dxchart5 from bugfix/DXCF-5832-resizing-settings-modal-issue to master
    
    Squashed commit of the following:
    
    commit e3be54f60e03f0627d735a9be6a1859ad85a71c6
    Author: Vanina Kalapsazova <vkalapsazova@devexperts.com>
    Date:   Fri Oct 24 14:37:55 2025 +0300
    
        [DXCF-5832] [The5ers][Web] Modal size of the settings menu remains full screen after resizing the browser back to desktop size. // add mobile and ua cache and change vp unit
    
    commit 2330e41475f2d82168d21e8faa44c913e033ea3e
    Author: Vanina Kalapsazova <vkalapsazova@devexperts.com>
    Date:   Thu Oct 23 13:46:14 2025 +0300
    
        [DXCF-5832] [The5ers][Web] Modal size of the settings menu remains full screen after resizing the browser back to desktop size. // update the popup position calculation when switching desktop to mobile and reverse
    
    commit 30dddb9b6c9b821418eb082bfb476dea471b5eff
    Author: Vanina Kalapsazova <vkalapsazova@devexperts.com>
    Date:   Tue Oct 21 10:54:42 2025 +0300
    
        [DXCF-5832] [The5ers][Web] Modal size of the settings menu remains full screen after resizing the browser back to desktop size. // update useMobile custom hook to improve layout mode detection
    
    GitOrigin-RevId: 2cd33c2dceced22b2c068990afda5f8d0825d80e

--- src/chart/utils/device/browser.utils.ts
+++ src/chart/utils/device/browser.utils.ts
@@ -3,13 +3,20 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
  * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
-let isMobileCache: boolean | undefined = undefined;
+let isMobileCache: boolean | undefined;
+let userAgentCache: string | undefined;
 /**
  * @doc-tags utility,mobile
  */
 export const isMobile = () => {
-	if (!isMobileCache) {
-		isMobileCache = !!navigator.userAgent.match(/Android|iPhone|Opera Mini/) || false;
+	if (typeof navigator === 'undefined') {
+		return false;
+	}
+
+	const currentUserAgent = navigator.userAgent;
+	if (userAgentCache !== currentUserAgent || isMobileCache === undefined) {
+		userAgentCache = currentUserAgent;
+		isMobileCache = !!currentUserAgent.match(/Android|iPhone|Opera Mini/);
 	}
 	return isMobileCache;
 };
