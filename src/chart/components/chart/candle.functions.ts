2c1c90f98e90f275f7f9ecaf2327c04761601fca
Anton Vorobev <avorobev@devexperts.com>
2026-02-03 11:07:02 +0000

    Pull request #6300: Feature/DXCF-6582 candle getters for time imp volatility open interest
    Merge in DXCHARTS/dxchart5 from feature/DXCF-6582-candle-getters-for-time-imp_volatility-open_interest to master
    
    Squashed commit of the following:
    
    commit 60a9353b173f6efa40fcc5a3c5f76dc6f2950d37
    Author: dxcity <dxcity@bots.devexperts.com>
    Date:   Tue Feb 3 10:20:59 2026 +0000
    
        CI: update snapshots
    
    commit 2b36441e855f31c56b9745eae01d5279893aebc0
    Merge: 645db4010e f581da71d6
    Author: Anton Vorobev <avorobev@devexperts.com>
    Date:   Mon Feb 2 17:31:05 2026 +0300
    
        Merge branch 'master' of ssh://stash.in.devexperts.com:7999/dxcharts/dxchart5 into feature/DXCF-6582-candle-getters-for-time-imp_volatility-open_interest
    
    commit 645db4010ef434dd074c232c1b58746da1f220a6
    Author: Anton Vorobev <avorobev@devexperts.com>
    Date:   Fri Jan 30 12:11:34 2026 +0300
    
        [DXCF-6582] Candle getters for time, imp_volatility, open_interest // small refactor
    
    commit 500e48a793a81467d66996a689f7ef95385ea78e
    Author: Anton Vorobev <avorobev@devexperts.com>
    Date:   Wed Jan 28 15:14:17 2026 +0300
    
        [DXCF-6582] Candle getters for time, imp_volatility, open_interest // init
    
    commit 7cc04df423069522bcb285643726b31fbde2e1d9
    Author: Anton Vorobev <avorobev@devexperts.com>
    Date:   Wed Jan 28 14:55:59 2026 +0300
    
        [DXCF-6582]
        Candle getters for time, imp_volatility, open_interest // init
    
    commit da33340941dd241e69001461fda3295de5aa37c5
    Author: Anton Vorobev <avorobev@devexperts.com>
    Date:   Tue Jan 27 16:13:39 2026 +0300
    
        [DXCF-6582] Candle getters for time, imp_volatility, open_interest // init
    
    commit 24e07547cd8d19018a23d00471b15b7aa5570424
    Author: Anton Vorobev <avorobev@devexperts.com>
    Date:   Tue Jan 27 15:37:44 2026 +0300
    
        [DXCF-6582] Candle getters for time, imp_volatility, open_interest // init
    
    commit 2cd2031ad6584c6f90f5c82e1747413b3c6aa489
    Author: Anton Vorobev <avorobev@devexperts.com>
    Date:   Tue Jan 27 14:16:12 2026 +0300
    
        [DXCF-6582] Candle getters for time, imp_volatility, open_interest // init
    
    commit 29fefb6a80d4bcda0efb58001f05458334f692c1
    Author: Anton Vorobev <avorobev@devexperts.com>
    Date:   Fri Jan 23 16:59:04 2026 +0300
    
        [DXCF-6582] Candle getters for time, imp_volatility, open_interest // init
    
    commit b9880ff0177996d37f2d6c6b4e7d370c50362e63
    Author: Anton Vorobev <avorobev@devexperts.com>
    Date:   Fri Jan 23 16:52:30 2026 +0300
    
        [DXCF-6582] Candle getters for time, imp_volatility, open_interest // init
    
    GitOrigin-RevId: 6bc89b04fc5eaa47f9d7a0417d3ccee0af534ac5

--- src/chart/components/chart/candle.functions.ts
+++ src/chart/components/chart/candle.functions.ts
@@ -1,10 +1,5 @@
 /*
- * Copyright (C) 2019 - 2025 Devexperts Solutions IE Limited
- * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
- * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
- */
-/*
- * Copyright (C) 2019 - 2025 Devexperts Solutions IE Limited
+ * Copyright (C) 2019 - 2026 Devexperts Solutions IE Limited
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
  * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
@@ -42,6 +37,7 @@ export const prepareCandle = (candle: PartialCandle): Candle | undefined => {
 			expansion: candle.expansion,
 			idx: candle.idx,
 			impVolatility: candle.impVolatility,
+			openInterest: candle.openInterest,
 			vwap: preparedVwap,
 			typicalPrice: candle.typicalPrice,
 		};
